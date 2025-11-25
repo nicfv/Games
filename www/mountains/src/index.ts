fa4c55b61f513aee746746921a052758ade97ed8
Nic <github@nicfv.com>
2025-11-24 21:08:49 -0800

    start working on mountain thing

---
+++ www/mountains/src/index.ts
@@ -0,0 +1,9 @@
+import { Canvas } from 'graphico';
+
+export const game: Canvas = new Canvas({
+    background: '#aabbcc',
+    border: 'black',
+    borderBlur: 'gray',
+    width: 1280,
+    height: 720,
+});
---
+++ www/mountains/src/mount.ts
@@ -0,0 +1,7 @@
+import { Drawable } from 'graphico';
+
+export class Mountain implements Drawable {
+    draw(graphics: CanvasRenderingContext2D): void {
+        throw new Error("Method not implemented.");
+    }
+}
