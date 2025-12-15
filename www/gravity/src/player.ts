32a8ae0e5ba85492715519e8e132595b9dab5a17
Nic <github@nicfv.com>
2025-12-15 13:02:13 -0800

    spawn at spawn point

--- www/gravity/src/player.ts
+++ www/gravity/src/player.ts
@@ -3,7 +3,10 @@ import { Direction, move, Vec2 } from './types';
 import { GameMap } from './map';
 
 export class Player implements Drawable {
-    constructor(private location: Vec2, private readonly map: GameMap) { }
+    private location: Vec2;
+    constructor(private readonly map: GameMap) {
+        this.location = map.spawnPoint;
+    }
     public movePlayer(direction: Direction): void {
         if (this.map.isWalkable(this.location, direction)) {
             this.location = move(this.location, direction);
@@ -11,10 +14,10 @@ export class Player implements Drawable {
     }
     public draw(graphics: CanvasRenderingContext2D): void {
         graphics.save();
-        graphics.translate(this.location.x, this.location.y);
+        graphics.translate(((graphics.canvas.width / 2) | 0) - this.location.x, ((graphics.canvas.height / 2) | 0) - this.location.y);
         this.map.draw(graphics);
         graphics.fillStyle = 'red';
-        graphics.fillRect(0, 0, 1, 1);
+        graphics.fillRect(this.location.x, this.location.y, 1, 1);
         graphics.restore();
     }
 }
