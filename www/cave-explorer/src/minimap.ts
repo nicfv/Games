f08e31e862c97904f5edfb3fd0f90c700540ab6d
Nic <github@nicfv.com>
2026-01-10 19:03:39 -0800

    location and size

--- www/cave-explorer/src/minimap.ts
+++ www/cave-explorer/src/minimap.ts
@@ -2,9 +2,10 @@ import { Drawable } from 'graphico';
 import { GameMap } from './map';
 import { Player } from './player';
 import { Collectibles } from './collectible';
+import { Vec2 } from './types';
 
 export class Minimap implements Drawable {
-    constructor(private readonly map: GameMap, private readonly player: Player, private readonly collectibles: Collectibles) { }
+    constructor(private readonly location: Vec2, private readonly size: Vec2, private readonly map: GameMap, private readonly player: Player, private readonly collectibles: Collectibles) { }
     public draw(graphics: CanvasRenderingContext2D): void {
         throw new Error('Method not implemented.');
     }
