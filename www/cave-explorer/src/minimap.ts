86f1951249cb875c331d5de2db102096c523dc90
Nic <github@nicfv.com>
2026-01-10 19:01:03 -0800

    construct with types

--- www/cave-explorer/src/minimap.ts
+++ www/cave-explorer/src/minimap.ts
@@ -1,6 +1,10 @@
 import { Drawable } from 'graphico';
+import { GameMap } from './map';
+import { Player } from './player';
+import { Collectibles } from './collectible';
 
 export class Minimap implements Drawable {
+    constructor(private readonly map: GameMap, private readonly player: Player, private readonly collectibles: Collectibles) { }
     public draw(graphics: CanvasRenderingContext2D): void {
         throw new Error('Method not implemented.');
     }
